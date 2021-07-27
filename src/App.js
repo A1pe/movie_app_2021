21b6acad9b9f6818ba41a8615935566e76fdda08
Eddy <46862196+A1pe@users.noreply.github.com>
2021-07-27 13:36:08 +0900

    update day1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import Potato from "./potato";
 
 function App() {
   return (
@@ -5,6 +7,7 @@ function App() {
       <h1>
         Hello~
       </h1>
+      <Potato />
     </div>
   );
 }
