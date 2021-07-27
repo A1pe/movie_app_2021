4d744699a35f7718db8763af616d3894634bd7e5
Eddy <46862196+A1pe@users.noreply.github.com>
2021-07-27 13:56:10 +0900

    updated lecture 2-1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-import Potato from "./potato";
+
+function Food({ fav }) {
+  return <h1>I like { fav }</h1>
+}
 
 function App() {
   return (
     <div>
-      <h1>
-        Hello~
-      </h1>
-      <Potato />
+      <h1>Hello~</h1>
+      <Food fav="Chicken"/>
+      <Food fav="Ramen"/>
+      <Food fav="Rice"/>
+      <Food fav="Bulgogi"/>
+      <Food fav="kimchi"/>
     </div>
   );
 }
