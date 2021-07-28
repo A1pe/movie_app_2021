1fe61ab07aeaabdbd14f5210edcffb6a6f078421
Eddy <46862196+A1pe@users.noreply.github.com>
2021-07-28 13:47:07 +0900

    Listened lecture 2.5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 // component area
 
-function Food({ name, source }) {
+function Food({ name, source, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
       <h3>that is from {source}</h3>
+      <h4>{rating}/5.0</h4>
     </div>
   );
 }
@@ -14,12 +16,17 @@ function Food({ name, source }) {
 // supported component area - map
 
 const foodILike = [
-  { id: 1, name: "Kimchi", source: "spice" },
-  { id: 2, name: "dongass", source: "fried" },
-  { id: 3, name: "bulgogi", source: "fire" },
-  { id: 4, name: "chicken", source: "fried" },
-  { id: 5, name: "rice", source: "rice" },
+  { id: 1, name: "Kimchi", source: "spice", rating: 5 },
+  { id: 2, name: "dongass", source: "fried", rating: 3 },
+  { id: 3, name: "bulgogi", source: "fire", rating: 4 },
+  { id: 4, name: "chicken", source: "fried", rating: 5 },
+  { id: 5, name: "rice", source: "rice", rating: 4 },
 ];
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  source: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 // main app area
 
@@ -28,7 +35,12 @@ function App() {
     <div>
       <h1>Hello~</h1>
       {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} source={dish.source} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          source={dish.source}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
