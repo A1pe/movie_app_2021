37f2fa433d87ccece1cb54953f1dc1a249e9b240
Eddy <46862196+A1pe@users.noreply.github.com>
2021-07-28 14:33:32 +0900

    Listened lecture 3.1
    learned state, setState, class component etc..

--- src/App.js
+++ src/App.js
@@ -3,47 +3,32 @@ import PropTypes from "prop-types";
 
 // component area
 
-function Food({ name, source, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h3>that is from {source}</h3>
-      <h4>{rating}/5.0</h4>
-    </div>
-  );
-}
-
 // supported component area - map
-
-const foodILike = [
-  { id: 1, name: "Kimchi", source: "spice", rating: 5 },
-  { id: 2, name: "dongass", source: "fried", rating: 3 },
-  { id: 3, name: "bulgogi", source: "fire", rating: 4 },
-  { id: 4, name: "chicken", source: "fried", rating: 5 },
-  { id: 5, name: "rice", source: "rice", rating: 4 },
-];
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  source: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
 
 // main app area
 
-function App() {
-  return (
-    <div>
-      <h1>Hello~</h1>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          source={dish.source}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    // Data can be changed in this area
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    console.log("minus");
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
